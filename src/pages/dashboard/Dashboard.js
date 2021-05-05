5b4ecb8d4c18766bfa727758c27e62aaf56a81d4
Leonardo Paganelli <leonardo.paganelli@iteris.com.br>
2021-05-04 21:30:22 -0300

    feature: add feature flag base

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -22,8 +22,24 @@ class Dashboard extends React.Component {
     this.state = {
       graph: null,
       checkedArr: [false, false, false],
+      showMap: false,
+      showMapStatistics: false,
+      showUserBaseGrowth: true,
+      showTrafficValue: true,
+      showRandomValues: true,
+      showMessages: true,
+      showMarketStats: true,
+      showCalendar: true,
     };
     this.checkTable = this.checkTable.bind(this);
+  }
+
+  componentDidMount() {
+    //TODO: integrate with flags
+    this.setState({
+      showMap: true,
+      showMapStatistics: false,
+    });
   }
 
   checkTable(id) {
@@ -57,130 +73,134 @@ class Dashboard extends React.Component {
     return (
       <div className={s.root}>
         <h1 className="page-title">
-          Dashboard &nbsp;
+          Dashboard {this.state.showMap} &nbsp;
           <small>
             <small>The Lucky One</small>
           </small>
         </h1>
 
         <Row>
-          <Col lg={7}>
-            <Widget className="bg-transparent">
-              <Map />
-            </Widget>
-          </Col>
-          <Col lg={1} />
+          {this.state.showMap && (
+            <Col lg={this.state.showMapStatistics ? 7 : 12}>
+              <Widget className="bg-transparent">
+                <Map />
+              </Widget>
+            </Col>
+          )}
+          {this.state.showMap && this.state.showMapStatistics && <Col lg={1} />}
 
-          <Col lg={4}>
-            <Widget
-              className="bg-transparent"
-              title={
-                <h5>
-                  {" "}
-                  Map
-                  <span className="fw-semi-bold">&nbsp;Statistics</span>
-                </h5>
-              }
-              settings
-              refresh
-              close
-            >
-              <p>
-                Status: <strong>Live</strong>
-              </p>
-              <p>
-                <span className="circle bg-default text-white">
-                  <i className="fa fa-map-marker" />
-                </span>{" "}
-                &nbsp; 146 Countries, 2759 Cities
-              </p>
-              <div className="row progress-stats">
-                <div className="col-md-9 col-12">
-                  <h6 className="name fw-semi-bold">Foreign Visits</h6>
-                  <p className="description deemphasize mb-xs text-white">
-                    Some Cool Text
-                  </p>
-                  <Progress
-                    color="primary"
-                    value="60"
-                    className="bg-subtle-blue progress-xs"
-                  />
+          {this.state.showMapStatistics && (
+            <Col lg={this.state.showMap ? 4 : 12}>
+              <Widget
+                className="bg-transparent"
+                title={
+                  <h5>
+                    {" "}
+                    Map
+                    <span className="fw-semi-bold">&nbsp;Statistics</span>
+                  </h5>
+                }
+                settings
+                refresh
+                close
+              >
+                <p>
+                  Status: <strong>Live</strong>
+                </p>
+                <p>
+                  <span className="circle bg-default text-white">
+                    <i className="fa fa-map-marker" />
+                  </span>{" "}
+                  &nbsp; 146 Countries, 2759 Cities
+                </p>
+                <div className="row progress-stats">
+                  <div className="col-md-9 col-12">
+                    <h6 className="name fw-semi-bold">Foreign Visits</h6>
+                    <p className="description deemphasize mb-xs text-white">
+                      Some Cool Text
+                    </p>
+                    <Progress
+                      color="primary"
+                      value="60"
+                      className="bg-subtle-blue progress-xs"
+                    />
+                  </div>
+                  <div className="col-md-3 col-12 text-center">
+                    <span className="status rounded rounded-lg bg-default text-light">
+                      <small>
+                        <AnimateNumber value={75} />%
+                      </small>
+                    </span>
+                  </div>
                 </div>
-                <div className="col-md-3 col-12 text-center">
-                  <span className="status rounded rounded-lg bg-default text-light">
-                    <small>
-                      <AnimateNumber value={75} />%
-                    </small>
+                <div className="row progress-stats">
+                  <div className="col-md-9 col-12">
+                    <h6 className="name fw-semi-bold">Local Visits</h6>
+                    <p className="description deemphasize mb-xs text-white">
+                      P. to C. Conversion
+                    </p>
+                    <Progress
+                      color="danger"
+                      value="39"
+                      className="bg-subtle-blue progress-xs"
+                    />
+                  </div>
+                  <div className="col-md-3 col-12 text-center">
+                    <span className="status rounded rounded-lg bg-default text-light">
+                      <small>
+                        <AnimateNumber value={84} />%
+                      </small>
+                    </span>
+                  </div>
+                </div>
+                <div className="row progress-stats">
+                  <div className="col-md-9 col-12">
+                    <h6 className="name fw-semi-bold">Sound Frequencies</h6>
+                    <p className="description deemphasize mb-xs text-white">
+                      Average Bitrate
+                    </p>
+                    <Progress
+                      color="success"
+                      value="80"
+                      className="bg-subtle-blue progress-xs"
+                    />
+                  </div>
+                  <div className="col-md-3 col-12 text-center">
+                    <span className="status rounded rounded-lg bg-default text-light">
+                      <small>
+                        <AnimateNumber value={92} />%
+                      </small>
+                    </span>
+                  </div>
+                </div>
+                <h6 className="fw-semi-bold mt">Map Distributions</h6>
+                <p>
+                  Tracking: <strong>Active</strong>
+                </p>
+                <p>
+                  <span className="circle bg-default text-white">
+                    <i className="fa fa-cog" />
+                  </span>
+                  &nbsp; 391 elements installed, 84 sets
+                </p>
+                <div className="input-group mt">
+                  <input
+                    type="text"
+                    className="form-control bg-custom-dark border-0"
+                    placeholder="Search Map"
+                  />
+                  <span className="input-group-btn">
+                    <button
+                      type="submit"
+                      className={`btn btn-subtle-blue ${s.searchBtn}`}
+                    >
+                      <i className="fa fa-search text-light" />
+                    </button>
                   </span>
                 </div>
-              </div>
-              <div className="row progress-stats">
-                <div className="col-md-9 col-12">
-                  <h6 className="name fw-semi-bold">Local Visits</h6>
-                  <p className="description deemphasize mb-xs text-white">
-                    P. to C. Conversion
-                  </p>
-                  <Progress
-                    color="danger"
-                    value="39"
-                    className="bg-subtle-blue progress-xs"
-                  />
-                </div>
-                <div className="col-md-3 col-12 text-center">
-                  <span className="status rounded rounded-lg bg-default text-light">
-                    <small>
-                      <AnimateNumber value={84} />%
-                    </small>
-                  </span>
-                </div>
-              </div>
-              <div className="row progress-stats">
-                <div className="col-md-9 col-12">
-                  <h6 className="name fw-semi-bold">Sound Frequencies</h6>
-                  <p className="description deemphasize mb-xs text-white">
-                    Average Bitrate
-                  </p>
-                  <Progress
-                    color="success"
-                    value="80"
-                    className="bg-subtle-blue progress-xs"
-                  />
-                </div>
-                <div className="col-md-3 col-12 text-center">
-                  <span className="status rounded rounded-lg bg-default text-light">
-                    <small>
-                      <AnimateNumber value={92} />%
-                    </small>
-                  </span>
-                </div>
-              </div>
-              <h6 className="fw-semi-bold mt">Map Distributions</h6>
-              <p>
-                Tracking: <strong>Active</strong>
-              </p>
-              <p>
-                <span className="circle bg-default text-white">
-                  <i className="fa fa-cog" />
-                </span>
-                &nbsp; 391 elements installed, 84 sets
-              </p>
-              <div className="input-group mt">
-                <input
-                  type="text"
-                  className="form-control bg-custom-dark border-0"
-                  placeholder="Search Map"
-                />
-                <span className="input-group-btn">
-                  <button
-                    type="submit"
-                    className={`btn btn-subtle-blue ${s.searchBtn}`}
-                  >
-                    <i className="fa fa-search text-light" />
-                  </button>
-                </span>
-              </div>
-            </Widget>
-          </Col>
+              </Widget>
+            </Col>
+          )}
         </Row>
 
         <Row>
